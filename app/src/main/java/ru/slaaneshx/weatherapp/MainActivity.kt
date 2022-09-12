f9da8ff236eafa90643083053a7c265b2e82ba1d
Alexander <tumakov.a@yandex.ru>
2022-09-12 20:55:51 +0300

    volley request/response test

--- app/src/main/java/ru/slaaneshx/weatherapp/MainActivity.kt
+++ app/src/main/java/ru/slaaneshx/weatherapp/MainActivity.kt
@@ -1,11 +1,39 @@
 package ru.slaaneshx.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+import ru.slaaneshx.weatherapp.databinding.ActivityMainBinding
+
+const val API_KEY = "1bdee3c77c0f428bb10103155221406"
+const val WEATHER_URL = "https://api.weatherapi.com/v1/current.json"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bGet.setOnClickListener {
+            getResponse()
+        }
+    }
+
+    private fun getResponse(name: String = "London") {
+        val url = "$WEATHER_URL?key=$API_KEY&q=$name&aqi=no"
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            { response ->
+                val responswJSON = JSONObject(response)
+                var temperature = responswJSON.getJSONObject("current").getString("temp_c")
+                Log.d("MyLog", temperature)
+            },
+            { Log.e("WeatherErrorLog", "Volley error $it") })
+        queue.add(stringRequest)
     }
 }
